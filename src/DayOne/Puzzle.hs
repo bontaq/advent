2eb98505547710ab009311222b6893cfe0e92598
Ian Davidson <bontaq@gmail.com>
2018-12-01 15:53:36 -0500

    day one done

---
+++ src/DayOne/Puzzle.hs
@@ -0,0 +1,36 @@
+module DayOne.Puzzle (partOne, partTwo) where
+
+import Text.Parser.Char
+import Text.Parser.Combinators
+import Text.Trifecta.Parser
+import Text.Parser.Token
+import qualified Data.Map.Strict as Map
+
+numbers :: Parser [Integer]
+numbers = many (token integer)
+
+partOne = do
+  readNumbers <- parseFromFile numbers "./src/DayOne/PartOneData.txt"
+  print $ readNumbers >>= pure . sum
+
+type Seen = Map.Map Integer Bool
+type Answer = Integer
+type Frequency = Integer
+
+findFrequency' :: Seen -> Frequency -> [Integer] -> Maybe Answer
+findFrequency' _ _ []           = Nothing
+findFrequency' seen freq (x:xs) =
+  let newFreq = x + freq
+  in case Map.lookup newFreq seen of
+    Just _  -> Just newFreq
+    Nothing -> do
+      let newSeen = Map.insert newFreq True seen
+      findFrequency' newSeen newFreq xs
+
+findFrequency :: [Integer] -> Maybe Answer
+findFrequency list = findFrequency' (Map.empty) 0 list
+
+partTwo :: IO ()
+partTwo = do
+  readNumbers <- parseFromFile numbers "./src/DayOne/PartOneData.txt"
+  print $ fmap (findFrequency . cycle) readNumbers
---
+++ src/Main.hs
@@ -0,0 +1,8 @@
+module Main where
+
+import qualified DayOne.Puzzle as DayOne
+
+main :: IO ()
+main = do
+  DayOne.partOne
+  DayOne.partTwo
