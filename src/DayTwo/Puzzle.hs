19dad619597b25485698b7d2b606e86844fd8311
Ian Davidson <ian.davidson@onepeloton.com>
2018-12-03 15:51:54 -0500

    day two part one

---
+++ src/DayTwo/Puzzle.hs
@@ -0,0 +1,29 @@
+module DayTwo.Puzzle (partOne) where
+
+import Text.Trifecta.Parser (Parser, parseFromFile)
+import Text.Parser.Combinators (many, eof, manyTill)
+import Text.Parser.Char (anyChar, newline)
+import Data.List
+
+words' :: Parser [Char]
+words' = manyTill anyChar newline
+
+score :: [[a]] -> (Int, Int)
+score characters =
+  let
+    scoreA = length . filter ((== 2) . length) $ characters
+    scoreB = length . filter ((== 3) . length) $ characters
+    scoreA' = if scoreA > 0 then 1 else 0
+    scoreB' = if scoreB > 0 then 1 else 0
+  in
+    (scoreA', scoreB')
+
+sumPairs :: [(Int, Int)] -> (Int, Int)
+sumPairs = foldr (\(a', b') (a, b) -> (a' + a, b' + b)) (0, 0)
+
+multiplyPair :: (Int, Int) -> Int
+multiplyPair (a, b) = a * b
+
+partOne = do
+  raw <- parseFromFile (many words')  "./src/DayTwo/Data.txt"
+  print $ (fmap $ multiplyPair . sumPairs) $ (fmap . fmap) (score . group . sort) $ raw
--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,10 @@
 module Main where
 
 import qualified DayOne.Puzzle as DayOne
+import qualified DayTwo.Puzzle as DayTwo
 
 main :: IO ()
 main = do
-  DayOne.partOne
-  DayOne.partTwo
+  -- DayOne.partOne
+  -- DayOne.partTwo
+  DayTwo.partOne
