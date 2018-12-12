77fbb4344f2564c9b7c53f50dd9aa7033802612e
Ian Davidson <ian.davidson@onepeloton.com>
2018-12-12 18:16:03 -0500

    Lovely parsing

--- src/DayFour/Puzzle.hs
+++ src/DayFour/Puzzle.hs
@@ -15,22 +15,66 @@ example = [r|
 [1518-11-02 23:56] Guard #3463 begins shift
 |]
 
-data Date = Date Integer Integer Integer
+type Id = Integer
 
-row :: Parser Integer
-row = do
-  optional newline
-  char '['
+data Date = Date Integer Integer Integer
+          deriving Show
+data Time = Time Integer Integer
+          deriving Show
+data DateTime = DateTime Date Time
+              deriving Show
+
+data Action = Sleep | WakeUp | Guard Id
+             deriving Show
+
+guard :: Parser Action
+guard = do
+  string "Guard #"
+  integer >>= pure . Guard
+
+wake :: Parser Action
+wake = do
+  string "wakes"
+  pure WakeUp
+
+sleep :: Parser Action
+sleep = do
+  string "falls"
+  pure Sleep
+
+data Row = Row DateTime Action
+         deriving Show
+
+dateParse :: Parser Date
+dateParse = do
   year <- integer
   char '-'
   month <- integer
   char '-'
   day <- integer
-  guardId <- try $ do
-    string " Guard #"
-    integer
+  pure $ Date year month day
+
+timeParse :: Parser Time
+timeParse = do
+  hour <- integer
+  char ':'
+  minute <- integer
+  pure $ Time hour minute
+
+row :: Parser Row
+row = do
+  optional newline
+
+  char '['
+  date <- dateParse
+  time <- timeParse
+  string "] "
+
+  action <- guard <|> wake <|> sleep
+
   optional (manyTill anyChar newline)
-  pure guardId
+
+  pure $ Row (DateTime date time) action
 
 partOne = do
   print $ parseString (many row) mempty example
