469c553418d5268b8f124eeb285263260f5edbb2
Ian Davidson <ian.davidson@onepeloton.com>
2018-12-13 14:48:30 -0500

    Add a window fn

--- src/DayFive/Puzzle.hs
+++ src/DayFive/Puzzle.hs
@@ -1,3 +1,13 @@
 module DayFive.Puzzle where
 
+import Control.Monad
+
+window :: Int -> [a] -> [[a]]
+window n xs
+  | length xs >= n = take n xs : window n (drop 1 xs)
+  | otherwise = []
+
+--
+-- How many units remain after fully reacting the polymer you scanned?
+--
 partOne = print "sup"
