1a04ad9fa9bb5c6db5d9ee1b536cdebb451721dd
Ian Davidson <ian.davidson@onepeloton.com>
2018-12-13 17:58:49 -0500

    kk

--- src/DaySix/Puzzle.hs
+++ src/DaySix/Puzzle.hs
@@ -1,1 +1,15 @@
 module DaySix.Puzzle where
+
+import Text.Parser.Combinators
+import Text.Parser.Char
+import Text.Parser.Token
+
+row = do
+  x <- integer
+  string ", "
+  y <- integer
+  (x, y)
+
+partOne = do
+  values <- lines . readFile
+  print ""
