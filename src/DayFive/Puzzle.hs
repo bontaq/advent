081d80d401ebab14a391679ad7eaa2cf7fa62086
Ian Davidson <ian.davidson@onepeloton.com>
2018-12-13 16:15:07 -0500

    part one day five smacked

--- src/DayFive/Puzzle.hs
+++ src/DayFive/Puzzle.hs
@@ -1,13 +1,36 @@
 module DayFive.Puzzle where
 
 import Control.Monad
+import Data.Char
 
 window :: Int -> [a] -> [[a]]
 window n xs
   | length xs >= n = take n xs : window n (drop 1 xs)
   | otherwise = []
 
+collapse a b
+  | isUpper a /= isUpper b = toUpper a == toUpper b
+  | otherwise = False
+
+pass :: [Char] -> [Char]
+pass (a:b:rest) =
+  case collapse a b of
+    True -> pass rest
+    False -> a : (pass (b : rest))
+pass a = a
+
+clean :: [Char] -> [Char]
+clean raw =
+  let cleaned = pass raw
+  in if cleaned == raw
+     then cleaned
+     else clean cleaned
+
 --
 -- How many units remain after fully reacting the polymer you scanned?
 --
-partOne = print "sup"
+partOne = do
+  -- amusingly seems to insert a newline?
+  input <- readFile "./src/DayFive/Data.txt"
+  print $ length input
+  print $ clean $ input
