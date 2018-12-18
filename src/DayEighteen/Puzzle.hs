c9435237385bae67b359c095d2cc8118e0de18a7
Ian Davidson <ian.davidson@onepeloton.com>
2018-12-18 16:54:17 -0500

    ok doke right where I was at... with day fifteen

--- src/DayEighteen/Puzzle.hs
+++ src/DayEighteen/Puzzle.hs
@@ -1,3 +1,42 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module DayEighteen.Puzzle where
+
+import Text.RawString.QQ (r)
+import Control.Lens
+
+start = [r|
+.#.#...|#.
+.....#|##|
+.|..|...#.
+..|#.....#
+#.#|||#|#|
+...#.||...
+.|....|...
+||...#|.#|
+|.||||..|.
+...#.|..|.
+|]
+
+data Tile = Forest | Lumberyard | Open
+instance Show Tile where
+  show Open       = "."
+  show Forest     = "|"
+  show Lumberyard = "#"
+
+type Board = [[Tile]]
+
+showBoard :: Board -> IO ()
+showBoard = mapM_ (print . (filter (/= ',')) . show)
+
+mkTile '.' = Open
+mkTile '#' = Lumberyard
+mkTile '|' = Forest
+
+mkBoard :: [Char] -> [[Tile]]
+mkBoard =
+  map (\row -> map (\c -> mkTile c) row) . tokenize
+  where
+    tokenize = filter ((> 0) . length) . lines
 
 partOne = print "sup"
