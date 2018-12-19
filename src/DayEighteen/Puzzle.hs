8c64fda1f972286d9510f74ba9069463971fa610
Ian Davidson <bontaq@gmail.com>
2018-12-19 02:31:51 -0500

    It's too slow, kill me

--- src/DayEighteen/Puzzle.hs
+++ src/DayEighteen/Puzzle.hs
@@ -71,19 +71,25 @@ collectTile ((Just t), c) = Just (t, c)
 collectTile (Nothing, _)  = Nothing
 
 collectTiles :: [(Maybe a, b)] -> [(a, b)]
-collectTiles = id $! catMaybes . map collectTile
+collectTiles tiles = catMaybes $ map collectTile tiles
 
---getSurrounding :: (Int, Int) -> Board -> [(Tile, (Int, Int))]
+coordAdjustments :: [(Int, Int)]
+coordAdjustments = [ (0, 0), (1, 0), (2, 0)
+                   , (0, 1),         (2, 1)
+                   , (0, 2), (1, 2), (2, 2) ]
+
+adjustedCoords :: (Num a, Num b) => a -> b -> [(a, b)] -> [(a, b)]
+adjustedCoords x y coords = map (\(x', y') -> ( x' + x - 1
+                                             ,  y' + y - 1) ) coords
+
 getSurrounding :: (Int, Int) -> Board -> Seq (Tile, (Int, Int))
 getSurrounding (x, y) board =
-  let coords = [ (0, 0), (1, 0), (2, 0)
-               , (0, 1),         (2, 1)
-               , (0, 2), (1, 2), (2, 2) ]
-      adjustedCoords = map (\(x', y') -> (x' + x - 1, y' + y - 1)) coords
+  let coords = coordAdjustments
+      adjustedCoords' = adjustedCoords x y coords
   in fromList
      $ collectTiles
      $ map (\(x, y) ->
-             ((board !? x) >>= \a -> a !? y, (x, y))) adjustedCoords
+             ((board !? x) >>= (flip (!?) y), (x, y))) adjustedCoords'
 
 getByTile :: Eq a => a -> Seq (a, b) -> Seq (a, b)
 getByTile !t = filter ((==) t . fst)
@@ -92,25 +98,24 @@ getNextTile :: Tile -> Seq (Tile, (Int, Int)) -> Tile
 getNextTile Open       surrounding
   | fs >= 3   = Forest
   | otherwise = Open
-  where !fs = length $ getByTile Forest surrounding
+  where fs = length $ getByTile Forest surrounding
 
 getNextTile Forest     surrounding
   | ls >= 3   = Lumberyard
   | otherwise = Forest
-  where !ls = length $ getByTile Lumberyard surrounding
+  where ls = length $ getByTile Lumberyard surrounding
 
 getNextTile Lumberyard surrounding
   | ls > 0 && fs > 0 = Lumberyard
   | otherwise = Open
-  where !ls = length $ getByTile Lumberyard surrounding
-        !fs = length $ getByTile Forest surrounding
+  where ls = length $ getByTile Lumberyard surrounding
+        fs = length $ getByTile Forest surrounding
 
 groupByX :: Eq a => [(a, b)] -> [[(a, b)]]
-groupByX = id $! groupBy (\(x, _) (x', _) -> x == x')
+groupByX = groupBy (\(x, _) (x', _) -> x == x')
 
--- withCoordinates :: Board -> [[(Tile, (Int, Int))]]
-withCoordinates :: Seq (Seq a) -> Seq (Seq (a, (Int, Int)))
-withCoordinates board =
+withCoordinates :: Board -> Seq (Seq (Tile, (Int, Int)))
+withCoordinates !board =
   let x = length board
       y = length $ fromJust (board !? 0)
       coords = groupByX $ liftA2 (,) ([0..x]) ([0..y])
@@ -128,11 +133,10 @@ runMinute !board =
     $ (map toList) $ toList withCoords
 
 runMinutes :: Int -> Board -> Board
-runMinutes 0 !b = b
-runMinutes !i !b =
-  let !newBoard = runMinute b
-      !newI = i - 1
-  in runMinutes newI newBoard
+runMinutes 0 b  = b
+runMinutes i b = do
+  let newBoard = runMinute b
+  runMinutes (i - 1) newBoard
 
 partOne = do
   board' <- readFile "./src/DayEighteen/Data.txt"
@@ -142,10 +146,9 @@ partOne = do
   print woodCount
   print lumberCount
   print (woodCount * lumberCount)
-  -- showBoard $ runMinutes 10 (mkBoard board')
 
 partTwo = do
-  board' <- id $! readFile "./src/DayEighteen/Data.txt"
+  board' <- readFile "./src/DayEighteen/Data.txt"
   let !ansBoard = runMinutes 10000 (mkBoard board')
       woodCount = length $ filter (== Forest) $ foldr (><) mempty ansBoard
       lumberCount = length $ filter (== Lumberyard) $ foldr (><) mempty ansBoard
