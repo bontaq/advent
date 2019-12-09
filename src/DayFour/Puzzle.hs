65ed9627d520bf66c467e92c85cc59aac1cb9276
Ian Davidson <ian.davidson@onepeloton.com>
2019-12-09 12:08:48 -0500

    np

--- src/DayFour/Puzzle.hs
+++ src/DayFour/Puzzle.hs
@@ -1,5 +1,8 @@
 module DayFour.Puzzle where
 
+import Data.List
+import Debug.Trace
+
 -- You arrive at the Venus fuel depot only to discover it's protected by a password. The Elves had written the password on a sticky note, but someone threw it out.
 --
 -- However, they do remember a few key facts about the password:
@@ -19,8 +22,16 @@ module DayFour.Puzzle where
 --
 -- Your puzzle input is 240920-789857.
 
+increaseRule :: String -> Bool
+increaseRule (a:b:cs) = (a <= b) && increaseRule (b:cs)
+increaseRule _ = True
+
+dupRule :: String -> Bool
+dupRule s = (> 0) . length $ filter (== 2) $ fmap length $ group s
+
 partOne :: IO ()
 partOne = do
-  let input = [2408920..789857]
+  let input = fmap show [240920..789857]
+      ans = length $ filter (\x -> dupRule x && increaseRule x) input
 
-  pure ()
+  print ans
