3a795b80d3eb9bb0cfd959ff8969d47131d0ba49
Ian Davidson <bontaq@gmail.com>
2019-12-18 04:04:56 -0500

    Neat but no good

--- src/DayEight/Puzzle.hs
+++ src/DayEight/Puzzle.hs
@@ -1,9 +1,21 @@
 module DayEight.Puzzle where
 
+
 -- 25 pixels wide and 6 pixels tall.
 
 type Width = Int
 type Height = Int
 
+infini f x = f x : infini f (f x)
+
 layerize :: Width -> Height -> [a] -> [[a]]
-layerize = undefined
+layerize x y p =
+  let grouped = take y $ infini (take x) p
+  in grouped
+
+testInput = "123456789012"
+
+parse :: String -> [Int]
+parse = fmap (\x -> read (x : "") :: Int)
+
+testLayerize = layerize 3 2 (parse testInput)
