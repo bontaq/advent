fd2375d5e122205015d2e817b5edb8188933fa82
Ian Davidson <bontaq@gmail.com>
2019-12-19 02:27:02 -0500

    Almost works and I hate it

--- src/DayNine/Puzzle.hs
+++ src/DayNine/Puzzle.hs
@@ -20,7 +20,7 @@ number = do
 numbers :: Parser [Int]
 numbers = many number
 
-data Mode = I | P | R
+data Mode = R | I | P
             deriving (Show, Eq)
 
 mode '2' = R
@@ -38,26 +38,56 @@ parseOp c =
           stupid = "0" <> (take (length s - 1) s)
           a = fromMaybe '0' $ stupid ^? element 0
           b = fromMaybe '0' $ stupid ^? element 1
+          c = fromMaybe '0' $ stupid ^? element 2
           z = drop ((length s) - 1) s
         in
-          ((read z :: Int), [mode a, mode b])
+          ((read z :: Int), [mode a, mode b, mode c])
     4 ->
         let
           stupid = take (length s - 2) s
           a = fromMaybe '0' $ stupid ^? element 0
           b = fromMaybe '0' $ stupid ^? element 1
+          c = fromMaybe '0' $ stupid ^? element 2
           z = drop ((length s) - 2) s
         in
-          ((read z :: Int), [mode a, mode b])
+          ((read z :: Int), [mode a, mode b, mode c])
+    5 ->
+      let
+        stupid = take ((length s) - 2) s
+        a = fromMaybe '0' $ stupid ^? element 0
+        b = fromMaybe '0' $ stupid ^? element 1
+        c = fromMaybe '0' $ stupid ^? element 2
+        z = drop ((length s) - 2) s
+      in
+        ((read z :: Int), [mode a, mode b, mode c])
     _ -> error s
 
 
 type Done = Bool
 type Offset = Int
+type Relative = Int
+type Adjust = Int
 type ProgramOut = (Done, Offset, Program, IO')
 
+                      -- v0 = case m2 of
+                      --   P -> program ! (program ! (offset + 1))
+                      --   I -> program ! (offset + 1)
+                      --   R -> program ! (program ! ((offset + 1) + relative))
+
+
+getValue :: Mode -> Program -> Offset -> Adjust -> Relative -> Int
+getValue m program offset adjust relative = case m of
+  R -> program ! ((program ! (offset + adjust)) + relative)
+  I -> program ! (offset + adjust)
+  P -> program ! (program ! (offset + adjust))
+
+getWrite :: Mode -> Program -> Offset -> Adjust -> Relative -> Int
+getWrite m program offset adjust relative = case m of
+  R -> (program ! (offset + adjust)) + relative
+  _ -> program ! (offset + adjust)
+
 runProgram :: Int -> Int -> Vector Int -> ([Int], [Int]) -> ProgramOut
--- runProgram offset program io | trace (show  " " <> show io) False = undefined
+runProgram offset relative program io | trace (show $ (show $ program ! offset) <> "::" <> show relative) False = undefined
 runProgram offset relative program io =
   let
     opcode = program ! offset
@@ -71,41 +101,49 @@ runProgram offset relative program io =
         where run = case op of
                 -- addition
                 1 ->
-                  let (m1:m2:_) = modes
-                      v0 = if m2 == I then program ! (offset + 1) else program ! (program ! (offset + 1))
-                      v1 = if m1 == I then program ! (offset + 2) else program ! (program ! (offset + 2))
+                  let (m1:m2:m3:_) = modes
+                      v0 = getValue m2 program offset 1 relative
+                      v1 = getValue m1 program offset 2 relative
+                      store = getWrite m3 program offset 3 relative
                   in
                     runProgram (offset + 4) relative (program // [(store, (+) v0 v1)]) io
 
                 -- multiply
                 2 ->
-                  let (m1:m2:_) = modes
-                      v0 = if m2 == I then program ! (offset + 1) else program ! (program ! (offset + 1))
-                      v1 = if m1 == I then program ! (offset + 2) else program ! (program ! (offset + 2))
+                  let (m1:m2:m3:_) = modes
+                      v0 = getValue m2 program offset 1 relative
+                      v1 = getValue m1 program offset 2 relative
+                      store = getWrite m3 program offset 3 relative
                   in
                     runProgram (offset + 4) relative (program // [(store, (*) v0 v1)]) io
 
                 -- store input
                 3 ->
                   let (ins, out) = io
+                      (m1:m2:m3:_) = modes
                   in case length ins of
                     0 -> (False, offset, program, io) -- return it for continuing to run
                     _ -> let (i:is) = ins
                          in
-                           runProgram (offset + 2) relative (program // [(program ! (offset + 1), i)]) (is, out)
+                          case m2 of
+                            R -> runProgram (offset + 2) relative (program // [(relative + (program ! (offset + 1)), i)]) (is, out)
+                            _ -> runProgram (offset + 2) relative (program // [(program ! (offset + 1), i)]) (is, out)
 
                 -- output
                 4 ->
                   let (i, o) = io
                       (m1:m2:_) = modes
-                      v0 = if m2 == I then program ! (offset + 1) else program ! (program ! (offset + 1))
+                      v0 = getValue m2 program offset 1 relative
+                      -- v0 = if m2 == I then program ! (offset + 1) else program ! (program ! (offset + 1))
                   in runProgram (offset + 2) relative program (i, v0 : o)
 
                 -- jump if true
                 5 ->
-                  let (m1:m2:_) = modes
-                      v0 = if m2 == I then program ! (offset + 1) else program ! (program ! (offset + 1))
-                      v1 = if m1 == I then program ! (offset + 2) else program ! (program ! (offset + 2))
+                  let (m1:m2:m3:_) = modes
+                      v0 = getValue m2 program offset 1 relative
+                      v1 = getValue m1 program offset 2 relative
+                      -- v0 = if m2 == I then program ! (offset + 1) else program ! (program ! (offset + 1))
+                      -- v1 = if m1 == I then program ! (offset + 2) else program ! (program ! (offset + 2))
                   in
                     if v0 /= 0
                     then runProgram v1 relative program io
@@ -113,9 +151,12 @@ runProgram offset relative program io =
 
                 -- jump if false
                 6 ->
-                  let (m1:m2:_) = modes
-                      v0 = if m2 == I then program ! (offset + 1) else program ! (program ! (offset + 1))
-                      v1 = if m1 == I then program ! (offset + 2) else program ! (program ! (offset + 2))
+                  let (m1:m2:m3:_) = modes
+                      v0 = getValue m2 program offset 1 relative
+                      v1 = getValue m1 program offset 2 relative
+                        -- R -> program ! (program ! ((offset + 2) + relative))
+                      -- v0 = if m2 == I then program ! (offset + 1) else program ! (program ! (offset + 1))
+                      -- v1 = if m1 == I then program ! (offset + 2) else program ! (program ! (offset + 2))
                   in
                     if v0 == 0
                     then runProgram v1 relative program io
@@ -123,9 +164,12 @@ runProgram offset relative program io =
 
                 -- less than
                 7 ->
-                  let (m1:m2:_) = modes
-                      v0 = if m2 == I then program ! (offset + 1) else program ! (program ! (offset + 1))
-                      v1 = if m1 == I then program ! (offset + 2) else program ! (program ! (offset + 2))
+                  let (m1:m2:m3:_) = modes
+                      v0 = getValue m2 program offset 1 relative
+                      v1 = getValue m1 program offset 2 relative
+                      store = getWrite m3 program offset 3 relative
+                      -- v0 = if m2 == I then program ! (offset + 1) else program ! (program ! (offset + 1))
+                      -- v1 = if m1 == I then program ! (offset + 2) else program ! (program ! (offset + 2))
                   in
                     if v0 < v1
                     then runProgram (offset + 4) relative (program // [(store, 1)]) io
@@ -133,15 +177,36 @@ runProgram offset relative program io =
 
                 -- equals
                 8 ->
-                  let (m1:m2:_) = modes
-                      v0 = if m2 == I then program ! (offset + 1) else program ! (program ! (offset + 1))
-                      v1 = if m1 == I then program ! (offset + 2) else program ! (program ! (offset + 2))
+                  let (m1:m2:m3:_) = modes
+                      v0 = getValue m2 program offset 1 relative
+                      v1 = getValue m1 program offset 2 relative
+                      store = getWrite m3 program offset 3 relative
+                        -- R -> program ! (program ! ((offset + 2) + relative))
+                      -- v0 = if m2 == I then program ! (offset + 1) else program ! (program ! (offset + 1))
+                      -- v1 = if m1 == I then program ! (offset + 2) else program ! (program ! (offset + 2))
                   in
                     if v0 == v1
                     then runProgram (offset + 4) relative (program // [(store, 1)]) io
                     else runProgram (offset + 4) relative (program // [(store, 0)]) io
 
-                e -> error $ show offset <> " " -- <> show program
+                9 ->
+                  let (m1:m2:_) = modes
+                      -- v0 = program ! (program
+                      v0 = getValue m2 program offset 1 relative
+                  in
+                    runProgram (offset + 2) (relative + v0) program io
+
+                e -> error $ show offset <> " " <> show op <> " " <> show program
 
 type IO' = ([Int], [Int])
 type Program = Vector Int
+
+partOne = do
+  f <- readFile "./src/DayNine/data.txt"
+  let
+    parsed = parseString numbers mempty f
+    -- fixed = fmap (\x -> fromList ((take 2000 (repeat 0)) <> x <> (take 2000 (repeat 0)))) parsed
+    fixed = fmap (\x -> fromList ( x <> (take 1000 (repeat 0)) )) parsed
+    ran = fmap (\x -> runProgram 0 0 x ([1], [])) fixed
+
+  print ran
