638335e6d50eeb9ee13937d027d484cf9591bd6a
Ian <bontaq@gmail.com>
2021-12-01 11:49:44 -0500

    A little different but better

--- src/2021/DayOne/Puzzle.hs
+++ src/2021/DayOne/Puzzle.hs
@@ -3,19 +3,22 @@ module DayOne.Puzzle where
 parseInt :: String -> Int
 parseInt = read
 
-numbers = fmap parseInt <$> lines <$> readFile "./src/2021/DayOne/data.txt"
+getNumbers = fmap parseInt <$> lines <$> readFile "./src/2021/DayOne/data.txt"
 
-countIncreases numbers =
-  subtract 1 -- the first one's always true because of the leading 0
-  $ length
-  $ filter (\(current, past) -> current > past)
-  $ (zip numbers (0:numbers))
+countIncreases' :: Int -> [Int] -> Int
+countIncreases' count (past:current:rest) =
+  let
+    newCount = if past < current then count + 1 else count
+  in
+    countIncreases' newCount (current:rest)
 
-partOne = print =<< countIncreases <$> numbers
+countIncreases' count _ = count
+
+countIncreases = countIncreases' 0
+
+partOne = print =<< countIncreases <$> getNumbers
 
 window (a:b:c:rest) = [a,b,c] : window (b:c:rest)
 window _ = []
 
-partTwo = do
-  numbers' <- fmap sum <$> window <$> numbers
-  print $ countIncreases numbers'
+partTwo = print =<< countIncreases <$> fmap sum <$> window <$> getNumbers
