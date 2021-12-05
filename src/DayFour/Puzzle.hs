92661a2c8484ef47258317ce0b079b2fac9bc911
Ian <bontaq@gmail.com>
2021-12-05 15:03:04 -0500

    Day four done

--- src/DayFour/Puzzle.hs
+++ src/DayFour/Puzzle.hs
@@ -1,20 +1,22 @@
 module DayFour.Puzzle where
 
+import Data.List
+
 -----------
 -- Model --
 -----------
 
 data Tile = Free Int | Called Int
-  deriving Show
+  deriving (Show, Eq)
 
 newtype Board = Board [[Tile]]
-  deriving Show
+  deriving (Show, Eq)
 
 -----------
 -- Parse --
 -----------
 
-fileLocation = "./src/DayFour/testData.txt"
+fileLocation = "./src/DayFour/data.txt"
 
 boardChunks :: [String] -> [[String]]
 boardChunks [] = []
@@ -33,4 +35,67 @@ parse = do
   (called:boards) <- lines <$> readFile fileLocation
   let boards' = parseBoards (drop 1 boards)
       called' = parseCalled called
-  pure boards'
+  pure (called', boards')
+
+--------------
+-- Part One --
+--------------
+
+setCalled :: Int -> [Board] -> [Board]
+setCalled number = fmap set
+  where set (Board board) = Board $ fmap setInner board
+        setInner row = fmap toggle row
+        toggle (Free num) = if num == number then Called num else Free num
+        toggle (Called num) = Called num
+
+rotateBoard (row:rows)= foldr combine (fmap (: []) row) rows
+  where combine toCombine acc = (\(a, b) -> b:a) <$> zip acc toCombine
+
+checkWin = any (all isCalled)
+  where
+    isCalled (Called _) = True
+    isCalled (Free _)   = False
+
+findWin [] = []
+findWin (Board board:boards) =
+  if checkWin board || checkWin (rotateBoard board)
+  then [board]
+  else findWin boards
+
+runCalls (call:nextCalls) boards =
+  let newBoards = setCalled call boards
+  in case findWin newBoards of
+    [winner] -> (call, winner)
+    [] -> runCalls nextCalls newBoards
+
+sumUncalled board = sum $ collectUncalled board
+  where collectUncalled xs = fmap toNumbers $ filter isUncalled $ mconcat xs
+        isUncalled (Free _)   = True
+        isUncalled (Called _) = False
+        toNumbers (Free x) = x
+
+partOne = do
+  (toCall, boards) <- parse
+  let (called, winningBoard) = runCalls toCall boards
+  print (called * sumUncalled winningBoard)
+
+--------------
+-- Part two --
+--------------
+
+findWins boards = foldr (<>) [] $ fmap isWinner boards
+  where isWinner (Board board) =
+          [board | checkWin board || checkWin (rotateBoard board)]
+
+runCalls' [] _ = []
+runCalls' (call:nextCalls) boards =
+  let newBoards = setCalled call boards
+      winners = Board <$> findWins newBoards
+  in (call, winners) : runCalls' nextCalls (newBoards \\ winners)
+
+partTwo = do
+  (toCall, boards) <- parse
+  let wins = filter (\(_, xs) -> not (null xs)) $ runCalls' toCall boards
+      (call, [Board lastWinner]) = last wins
+      x = sumUncalled lastWinner
+  print (call * x)
