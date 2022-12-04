532ff551cb60e8736c5657b63b5bf4dabdcd9400
Ian <bontaq@gmail.com>
2022-12-04 00:53:46 -0500

    Day four done

---
+++ src/DayFour/Puzzle.hs
@@ -0,0 +1,39 @@
+module DayFour.Puzzle where
+
+import Data.List
+import Data.List.Split
+
+parsePart :: String -> [Int]
+parsePart input =
+  let [start, end] = read <$> splitOn "-" input
+  in [start..end]
+
+parseLine line =
+  let parts = splitOn "," line
+  in
+    fmap parsePart parts
+
+input = fmap parseLine . lines <$> readFile "./src/DayFour/data.txt"
+
+fullyContained [a, b] = null (a \\ b) || null (b \\ a)
+
+partOne = do
+  ranges <- input
+
+  let
+    contained = fmap fullyContained ranges
+    containedCount = length $ filter (== True) contained
+
+
+  print containedCount
+
+partiallyContained [a, b] = (> 0) $ length $ a `intersect` b
+
+partTwo = do
+  ranges <- input
+
+  let
+    contained = fmap partiallyContained ranges
+    containedCount = length $ filter (== True) contained
+
+  print containedCount
